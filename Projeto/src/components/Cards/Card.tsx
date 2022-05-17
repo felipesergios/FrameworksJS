26d9b9e19e3efacc7350f4080b4ef1be4202d847
Felipe Sergio <felipesilva@MacBook-Pro-de-Felipe.local>
2022-05-17 11:46:59 -0300

    create componnent table view in reactjs

---
+++ Projeto/src/components/Cards/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+interface Cards{
+    Content:string;
+    backgroundColor?:string;
+    borderRadius?:Number;
+   
+}
+
+export default function Card(props:Cards){
+    return (
+<Container style={{ width: '18rem' }}>
+        <div style={{backgroundColor:props.backgroundColor,borderRadius:`${props.borderRadius}px`}}>
+        <h1>{props.Content}</h1>
+        </div>
+       
+</Container>
+    )
+}
