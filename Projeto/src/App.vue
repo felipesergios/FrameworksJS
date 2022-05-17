9c56f33308b4726b552a74f9cbd3335c435338e9
Felipe Sergio <felipesilva@MacBook-Pro-de-Felipe.local>
2022-05-17 11:21:37 -0300

    create componnent table view

---
+++ Projeto/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Tabs from './components/Tabs.vue';
+//import Quiz from './components/Quiz.vue'
+
+</script>
+
+<template>
+  <Tabs></Tabs>
+</template>
+
+<style>
+
+@import './assets/base.css';
+</style>
